031b14ab917496f2ac32f012237fcaa77c7b2f1e
SAI SUNIL AKURATHI <saisunilakurathi@gmail.com>
2024-08-02 12:32:31 -0400

    hero name change

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -3,19 +3,22 @@ export default function AboutMe() {
     return (
         <section id="AboutMe" className="about--section">
             <div className="about--section--img">
-                <img src="./img/about-me.png" alt="About Me" />
+                <img src="./img/hero_img.png" alt="About Me" />
             </div>
             <div className="hero--section--content--box about--section--box">
                 <div className="hero--section--content">
-                    <p className="section--titile">About</p>
+                    <p className="section--titile"></p>
                     <h1 className="skills--section--heading">
                         About Me
                     </h1>
                     <p className="hero--section--description">
-                    The href attribute is required for an anchor to be keyboard accessible. Provide a valid, navigable address as the href value. If you cannot provide an href,
+                        I have a strong foundation in programming principles, and I am proficient in multiple programming languages and frameworks. I enjoy solving complex problems and creating innovative solutions, and I am always striving to stay up-to-date with the latest technologies and trends in the industry.
                     </p>
                     <p className="hero--section--description">
-                    The href attribute is required for an anchor to be keyboard accessible. Provide a valid, navigable address as the href value.
+                        I am a proactive and self-motivated individual, and I take pride in my work and the quality of code that I produce. I am also a strong communicator and team player, and I understand the importance of effective collaboration in the software development process.
+                    </p>
+                    <p className="hero--section--description">
+                        In addition to my technical skills, I am also committed to maintaining a high level of professionalism, and I always aim to meet deadlines and exceed expectations. Overall, I am passionate about software development, and I am constantly looking for new challenges and opportunities to grow as a developer.
                     </p>
                 </div>
             </div>
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,20 +1,19 @@
 export default function HeroSection(){
     return (
         <section id="heroSection" className="hero--section">
-        <div className="hero--section--img">
-            <img src="./img/hero_img.jpg" alt="Hero Section"/>
-        </div>
+            {/* <div className="hero--section--img">
+                <img src="./img/hero_img.jpg" alt="Hero Section"/>
+            </div> */}
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
-                    <p className="section-title">Sai Sunil Akurathi</p>
+                    <p className="section-title">Sai Sunil Akurathi1</p>
                     <h1 className="hero--section--title">
                         <span className="hero--section--title--color">Full Stack</span>{ " " }
                         <br/>
                         Developer
                     </h1>
                     <p className="hero--section--description">
-                        Java, Node.js, Python, PHP
-                        <br/> HTML5, CSS3, JavaScript, jQuery, Ajax, React, Next.js
+                        As a full stack software developer, I am dedicated to producing high-quality code, continuously learning, improving, and collaborating with teams to deliver exceptional software solutions.
                     </p>
                 </div>
                 <button className="btn btn-primary">Get In Touch</button>
